03cb96752a9451f3611c570aac917b3d0678b62c
Carolina <carolinavj21@gmail.com>
2022-12-01 19:06:27 -0300

    se añade style a input

--- src/main.js
+++ src/main.js
@@ -1,17 +1,11 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+
 
 console.log(example, data);
 // console.log(data.films[0].poster)
 
 const prueba = document.getElementById("prueba")
-
-// prueba.innerHTML = `
-// <p class= "prueba"> ${data.films[0].poster}</p>
-// <img src="${data.films[0].poster}" />`
-
 
 
 const movies = data.films
