f60edb9669a3223f27e66323cce21c412dbaf540
Carolina <carolinavj21@gmail.com>
2022-12-01 15:09:04 -0300

    respaldo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+import data from './data/ghibli/ghibli.js';
+
+//console.log(data.films[0].poster);
+//console.log(example, data);
+
+const root = document.getElementById("peliculas")
+root.classList="poster-style"
+root.innerHTML = `
+`
+
+const peliculas =data.films
+console.log(data.films[0].title);
+
+const generadorHTML=(peliculas)=> {
+    const div = document.createElement("div")
+
+    const titlepelicula = document.createElement ("titlepelicula")
+    titlepelicula.textContent=peliculas.title
+
+    const img = document.createElement("img")
+    img.setAttribute("src", peliculas.poster)
+
+    div.append(img, titlepelicula)
+    return div
+}
+peliculas.forEach(pelicula=>root.appendChild(generadorHTML(pelicula)))
