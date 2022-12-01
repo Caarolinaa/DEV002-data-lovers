0522db77e022641ef1aba150ee612254cabc75aa
BelenCiel <belenaranjonoeck@icloud.com>
2022-12-01 14:40:16 -0300

    cambios en HTML y CSS. Agregar data (poster, titulo) a js y html

--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+console.log(data.films[0].poster)
+
+const prueba = document.getElementById("prueba")
+prueba.classList="poster-style"
+
+// prueba.innerHTML = `
+// <p class= "prueba"> ${data.films[0].poster}</p>
+// <img src="${data.films[0].poster}" />`
+
+
+
+const movies = data.films
+console.log(data.films[0].title);
+
+const generadorHTML=(movies)=> {
+    const div = document.createElement("div")
+
+    const movieTitle = document.createElement ("movieTitle")
+    movieTitle.textContent=movies.title
+
+    const img = document.createElement("img")
+    img.setAttribute("src", movies.poster)
+
+    div.append(img, movieTitle)
+    return div
+}
+movies.forEach(pelicula=>prueba.appendChild(generadorHTML(pelicula)))
