21645864728647cc8b7ee0a98a9f0112466e966f
BelenCiel <belenaranjonoeck@icloud.com>
2022-12-06 12:39:26 -0300

    comentarios

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import data from './data/ghibli/ghibli.js';
 console.log(example, data);
 // console.log(data.films[0].poster)
 
+//variable que contiene el contenido de html
 const prueba = document.getElementById("prueba")
 
-
+//variable que contiene la data de las peliculas
 const movies = data.films
 // console.log(data.films[0].title);
 
+//Variable que contiene los arrays de films
 const generadorHTML=(movies)=> {
     const div = document.createElement("article")
 
@@ -19,8 +21,10 @@ const generadorHTML=(movies)=> {
 
     const img = document.createElement("img")
     img.setAttribute("src", movies.poster)
-
+    
+     //append permite agregar objetos en cadena
     div.append(img, movieTitle)
     return div
 }
+//forEach ejecuta la funcion indicada por cada elemento del array
 movies.forEach(pelicula=>prueba.appendChild(generadorHTML(pelicula)))
