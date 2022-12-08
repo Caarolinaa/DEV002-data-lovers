5d5c6abb298b75b7e2e183bedb1f3332f1904acf
BelenCiel <belenaranjonoeck@icloud.com>
2022-12-08 14:20:46 -0300

    actualizacion archivos

--- src/data.js
+++ src/data.js
@@ -1,11 +1,10 @@
-// Creamos constante para filtrar peliculas por año mediante boton select
-export const dataMovies = (data, option) => { //const filterDate
-const dateSort = data.sort((a, b) => {
-  return a.release_date - b.release_date
-})
-  };
+export const dataMovies = () => {
 
-  /*export const anotherExample = () => {
+    return 'dataMovies';
+  };
+  
+  
+  export const anotherExample = () => {
     return 'OMG';
-  };*/
+  };
   
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-//import { dataMovies } from './data.js';
+import { dataMovies } from './data.js';
 import data from './data/ghibli/ghibli.js';
 
-//console.log(dataMovies, data);
+console.log(dataMovies, data);
+// console.log(data.films[0].poster)
 
 // const moviesPoster = document.getElementById("home2")
 // document.getElementById("home1").style.visibility = "visible";
@@ -15,11 +16,11 @@ function mostrarPantalla(){
 }
 
 const movies = data.films
+// console.log(data.films[0].title);
 
 //Variable que contiene los arrays de films
 const generadorHTML=(movies)=> {
     const div = document.createElement("article")
-    div.setAttribute("class", "articlemovie")
     
     const movieTitle = document.createElement("h3")
     movieTitle.setAttribute("class", "titleh3")
@@ -28,11 +29,13 @@ const generadorHTML=(movies)=> {
     const img = document.createElement("img")
     img.setAttribute("class", "imgposter")
     img.setAttribute("src", movies.poster)
-    
 
     div.append(img, movieTitle)
     return div
 }
 movies.forEach(pelicula=>moviesPoster.appendChild(generadorHTML(pelicula)))
 
-// Filtrar películas por año
+// const peliculaFiltrada = movies.filter (function(el) {
+//     return (el.title[0])
+// })
+// console.log(peliculaFiltrada)
