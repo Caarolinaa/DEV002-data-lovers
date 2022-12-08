847e8c5b04598b4ff4de5a10db11678e104ddac2
Carolina <carolinavj21@gmail.com>
2022-12-08 11:18:50 -0300

    se añade clase a article para alinear peliculas

--- src/data.js
+++ src/data.js
@@ -1,17 +1,6 @@
 export const dataMovies = (data, release_date) => {
-  //QUEREMOS TRANAJAR SOLO CON LA FECHA DE LANZAMIENTO -ESTA BIEN ESCRITO LA LINEA 1;
-  //COMO ORDENAR POR AÑO DE LANZAMIENTO  -- FILTER()
-  //COMO ORDENAR POR ORDEN ALFABETICO --SORT()
   
-  
-  //   const moviesFilter = data.filter(function (el) {
-  //     return (el.release_date[3])
-  //   }
-  //   )
-  //   //return 'dataMovies';
   };
-  // console.log(moviesFilter);
-  
   export const anotherExample = () => {
     return 'OMG';
   };
--- src/main.js
+++ src/main.js
@@ -2,13 +2,11 @@ import { dataMovies } from './data.js';
 import data from './data/ghibli/ghibli.js';
 
 console.log(dataMovies, data);
-// console.log(data.films[0].poster)
 
 const moviesPoster = document.getElementById("moviesPoster")
 
 
 const movies = data.films
-// console.log(data.films[0].title);
 
 //Variable que contiene los arrays de films
 const generadorHTML=(movies)=> {
@@ -21,16 +19,9 @@ const generadorHTML=(movies)=> {
     const img = document.createElement("img")
     img.setAttribute("class", "imgposter")
     img.setAttribute("src", movies.poster)
-
-    const date = data.release_date
     
 
     div.append(img, movieTitle)
     return div
 }
 movies.forEach(pelicula=>moviesPoster.appendChild(generadorHTML(pelicula)))
-
-// const peliculaFiltrada = movies.filter (function(el) {
-//     return (el.title[0])
-// })
-// console.log(peliculaFiltrada)
