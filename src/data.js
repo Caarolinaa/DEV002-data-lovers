8cd172f826c8c5e5ac409cbc4c3f65f2df42bc4f
Carolina <carolinavj21@gmail.com>
2022-12-12 13:55:15 -0300

    se agrega funcion de filtrado y buscador sin funcionalidad aun

--- src/data.js
+++ src/data.js
@@ -1,11 +1,37 @@
 // Creamos constante para filtrar peliculas por año mediante boton select
-export const dataMovies = (data, option) => { //const filterDate
-const dateSort = data.sort((a, b) => {
-  return a.release_date - b.release_date
-})
-  };
+//export const dataMovies = (data, option) => { //const filterDate
+//const dateSort = dataMovies.sort((a, b) => {
+  //if (option == 1){
+    //ordenar ascendente
+  //} else{
+    //ordenar descendente
+  //}
+  //return a.release_date - b.release_date
+//})
+  //};
+
 
   /*export const anotherExample = () => {
     return 'OMG';
   };*/
-  
+
+export const dataMovies = (release_date) => {
+  return movies.filter((film) => {
+    return film.release_date === release_date;
+  });
+}
+
+  export const dataMoviesAsc = (movies) => {      
+    return movies.sort((a,b) => {
+      if (a.release_date < b.release_date) {return -1}    
+    });    
+  }
+
+  export const dataMoviesDesc= (movies) => {
+    return movies.sort((a,b) => {
+      if (a.release_date > b.release_date) {return 1}    
+    });
+  }
+
+  //la fecha release date es string, se pasa a number usando parseInt para que pueda ordenarlos por fecha Asc o Desc
+  //luego de usar Parseint pasar a filtrado
